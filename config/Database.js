abad632f8ddfc0d9340eb55710ce3c89144be129
Jeslyn Vicky H. <162588624+jeslynvicky@users.noreply.github.com>
2025-03-22 11:23:38 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 const db = new Sequelize('notes','root','inipasswordtugas3',{
-    host: '35.232.218.92',
+    host: '34.135.185.104',
     dialect: 'mysql'
 })
 
